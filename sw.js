2bdb391e6d7e7b84e4dd0792d9b5da119f89fd19
Takenoko <takenoko.tech@gmail.com>
2019-08-12 22:51:14 +0900

    Updates

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ console.log('[ServiceWorker] loading...');
 
 // インストール処理 //
 self.addEventListener('install', event => {
+  console.log('[ServiceWorker] install');
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       console.log('[ServiceWorker] Pre-caching offline page');
@@ -15,6 +16,7 @@ self.addEventListener('install', event => {
 
 // リソースフェッチ時のキャッシュロード処理 //
 self.addEventListener('fetch', event => {
+  console.log('[ServiceWorker] fetch');
   event.respondWith(
     caches.match(event.request).then(response => {
       return response ? response : fetch(event.request);
